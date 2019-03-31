afd1b22d6f4bcde6f8ee66413070a0bd4939fb4d
Kyle Hennig <henn.kyle@outlook.com>
2019-03-31 13:34:20 -0600

    Schedule push notification on button press

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    LocalNotifications,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SettingsService } from '../settings.service';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,20 @@ import { SettingsService } from '../settings.service';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage {
-  constructor(public settings: SettingsService) { }
+  constructor(
+    public settings: SettingsService,
+    private localNotifications: LocalNotifications) { }
 
   openWebsite() {
-    window.open('https://lecdea.ca/',  '_blank');
+    window.open('https://lecdea.ca/', '_blank');
+  }
+
+  scheduleNotification() {
+    // Schedules a delayed notification.
+    this.localNotifications.schedule({
+      text: 'RABAIS!!!',
+      trigger: { at: new Date(new Date().getTime() + 5000) },
+      led: 'FF0000',
+    });
   }
 }
