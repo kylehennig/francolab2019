8f8e1d34b05fae286fe794458f66a10a85eabb7e
Kyle Hennig <henn.kyle@outlook.com>
2019-03-31 09:08:50 -0600

    Show only the city and region if address not provided

--- src/app/list-item/list-item.page.ts
+++ src/app/list-item/list-item.page.ts
@@ -25,6 +25,16 @@ export class ListItemPage implements OnInit {
     this.companies.sort((a, b) => a.company.localeCompare(b.company, 'fr'));
   }
 
+  fullAddress(id: number): string {
+    const company = this.server.getCompany(id);
+    let fullAddress = '';
+    if (company.address !== '') {
+      fullAddress += company.address + ', ';
+    }
+    fullAddress += company.city + ', ' + company.region;
+    return fullAddress;
+  }
+
   digitsOnly(input: string) {
     const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let output = '';
