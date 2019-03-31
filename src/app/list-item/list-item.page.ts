53452e541f8f38a8d5fe45a8c3d8d64a32379e62
Kyle Hennig <henn.kyle@outlook.com>
2019-03-31 01:54:15 -0600

    Show information about each category when clicked

--- src/app/list-item/list-item.page.ts
+++ src/app/list-item/list-item.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Company } from '../company';
+import { ServerService } from '../server.service';
 
 @Component({
   selector: 'app-list-item',
@@ -8,9 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ListItemPage implements OnInit {
   category: string;
-  constructor(private activatedRoute: ActivatedRoute) { }
+  companies: Company[] = [];
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private server: ServerService) { }
 
   ngOnInit() {
     this.category = this.activatedRoute.snapshot.paramMap.get('item');
+    const companyIds = this.server.getByCategory().get(this.category);
+    companyIds.forEach(id => {
+      const company = this.server.getCompany(id);
+      this.companies.push(company);
+    });
   }
 }
